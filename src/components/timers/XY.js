85348343d5c99d6e4f767baca9d9a20d26ff3313
jbhammett <jennifer.beakley@gmail.com>
2023-10-29 21:46:08 -0400

    Update XY.js

--- src/components/timers/XY.js
+++ src/components/timers/XY.js
@@ -8,7 +8,7 @@ const XY = () => {
 
 const [displayMinutesCount, setDisplayMinutesCount] = useState('00' );
 const [displaySecondsCount, setDisplaySecondsCount] = useState('00');
-const [displayRounds, setDisplayRounds] = useState(0);
+const [displayRounds, setDisplayRounds] = useState(1);
 
 
 const [startMinutes, setStartMinutes] = useState('00');
@@ -34,61 +34,84 @@ const handleSecondsInput = v => {
 
 const handleRoundsInput = v => {
     setRounds(v);
+    // setDisplayRounds(v);
 };
 
-const handleStartButton = (value) => {
-    
 
+const handleStartButton = (value) => {
+        
     let seconds = (parseInt(startMinutes * 60)) + parseInt(startSeconds);
-    setCounter(seconds);
+    if (parseInt(displayMinutesCount) * 60 + parseInt(displaySecondsCount) === seconds){
+        setCounter(seconds);
+    }
 
     totalSeconds.current = seconds;
 
-    if (totalSeconds.current > 0){
-        if (displayMinutesCount > 0){
+
+
+    if (totalSeconds.current > 0 && rounds > 0){
+        if (displayMinutesCount > 0 && displaySecondsCount === 0){
             setDisplayMinutesCount((displayMinutesCount - 1).toString().padStart(2,"0"));
         }
     // Start timer
-        for (let roundNum=0; roundNum<rounds; roundNum++){
-            secondsCountInterval.current = setInterval(() => {
-                setCounter((prevTotalSecondsCount) => {
-                    const nextTotalSecondsCounter = prevTotalSecondsCount - 1;
-            
-                    if (nextTotalSecondsCounter === 0) {
-                        clearInterval(secondsCountInterval.current);
-                        if (displayRounds > 0){
-                            setDisplayRounds((prevDisplayRoundsCount) => {
-                                const nextDisplayRoundsCount = prevDisplayRoundsCount - 1;
-                                return nextDisplayRoundsCount;
-                            });    
-                        }
+        secondsCountInterval.current = setInterval(() => {
+            let nextTotalSecondsCounter = 0;
+            setCounter((prevTotalSecondsCount) => {
+                if (prevTotalSecondsCount > 0) {
+                    nextTotalSecondsCounter = prevTotalSecondsCount - 1;
+                }
+                else if (displayRounds < rounds){
+        
+                    setDisplayRounds((prevRound) =>{
+                 
+                            setDisplayMinutesCount(startMinutes);
+                            setDisplaySecondsCount(startSeconds);
+                            const nextRound = prevRound + 1;
 
-                    }
+                            if (nextRound > rounds){
+                                nextTotalSecondsCounter = 0;
+                                setDisplayMinutesCount('00');
+                                setDisplaySecondsCount('00');
 
-                    if (nextTotalSecondsCounter % 60 === 0){
-                            setDisplayMinutesCount((prevDisplayMinutesCount) => {
-                                if(prevDisplayMinutesCount > 0){
-                                    let nextDisplayMinutesCount = parseInt(prevDisplayMinutesCount) - 1;
-                                    nextDisplayMinutesCount = nextDisplayMinutesCount.toString().padStart(2,"0");
-                                    return nextDisplayMinutesCount;
-                                }
-                                else {
-                                    return '00';        
-                                }
-                            });
+                                clearInterval(secondsCountInterval.current);
+                                return prevRound;
+                            }
+                            else {
+                                nextTotalSecondsCounter = totalSeconds.current;
+                                setDisplayMinutesCount(startMinutes);
+                                setDisplaySecondsCount(startSeconds);
+                                return nextRound;
+                            }
+                
+                    });
                     
-                        setDisplaySecondsCount('00');
+                    
+                }
+        
+                if (nextTotalSecondsCounter % 60 === 0){
+                    setDisplayMinutesCount((prevDisplayMinutesCount) => {
+                        if(prevDisplayMinutesCount > 0){
+                            let nextDisplayMinutesCount = parseInt(prevDisplayMinutesCount) - 1;
+                            nextDisplayMinutesCount = nextDisplayMinutesCount.toString().padStart(2,"0");
+                            return nextDisplayMinutesCount;
+                        }
+                        else {
+                            return '00';        
+                        }
+                    });
+                
+                    setDisplaySecondsCount('00');
 
-                    }
-                    else{                    
-                        let nextTotalSeconds = nextTotalSecondsCounter % 60;
-                        setDisplaySecondsCount(nextTotalSeconds.toString().padStart(2,"0"));
-                        
-                    }
-                    return nextTotalSecondsCounter;
-                });
-            }, 100);
-        }
+                }
+                else{                    
+                    let nextTotalSeconds = nextTotalSecondsCounter % 60;
+                    setDisplaySecondsCount(nextTotalSeconds.toString().padStart(2,"0"));
+                    
+                }
+                return nextTotalSecondsCounter;
+            });
+        
+        }, 100);
         
     }
   };
@@ -110,6 +133,8 @@ const handleStartButton = (value) => {
     setStartMinutes('00');
     setStartSeconds('00');
     setCounter(0);
+    setRounds(0);
+    setDisplayRounds(1);
     totalSeconds.current = 0;
     if (secondsCountInterval.current) {
         clearInterval(secondsCountInterval.current);
@@ -120,6 +145,7 @@ const handleStartButton = (value) => {
   const handleEndButton = (value) => {
     setDisplayMinutesCount(startMinutes);
     setDisplaySecondsCount(startSeconds);
+    setDisplayRounds(rounds);
     setCounter(totalSeconds.current);
     if (secondsCountInterval.current) {
         clearInterval(secondsCountInterval.current);
@@ -140,6 +166,9 @@ return (
 
             <div>
                 Counter: {counter}
+            </div>
+            <div>
+                Rounds: {displayRounds}
             </div>
             {/* COLOR DOES NOT WORK  */}
             <Button value={"Start"} color={'#00cccc'} onClick={handleStartButton} /> 
