4ee909780ac6dde0276a9f306749487c58f44a06
jbhammett <jbhammett@users.noreply.github.com>
2023-11-02 06:19:45 +0000

    deploy: 19eafb5e614afed010eac710b5b61d62ca6820b6

---
+++ precache-manifest.f8467efab1efdc6494eb99d642eecb81.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c3cdd50b4acdab69137ae52054a9a5c8",
+    "url": "/assignment-1-jbhammett/index.html"
+  },
+  {
+    "revision": "b6cf87539a0c852cdd63",
+    "url": "/assignment-1-jbhammett/static/css/main.02971024.chunk.css"
+  },
+  {
+    "revision": "5d78ca0e159a41b70ebe",
+    "url": "/assignment-1-jbhammett/static/js/2.0ba0deef.chunk.js"
+  },
+  {
+    "revision": "b6cf87539a0c852cdd63",
+    "url": "/assignment-1-jbhammett/static/js/main.48995be6.chunk.js"
+  },
+  {
+    "revision": "a80b6928c81b6c90be88",
+    "url": "/assignment-1-jbhammett/static/js/runtime~main.58656858.js"
+  }
+]);
